2ec471f9cd45ce2d83584d336bf4bba9b91604f8
ZKDS <nikitoc0@mail.ru>
2024-03-20 16:35:11 +0300

    Добавьте файлы проекта.

---
+++ 3_10_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace example_3_10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Добро пожаловать в счетчик карт в игре 21");
+            Write("Введите колличество обычных карт в руке");
+            int quantity = int.Parse(ReadLine());
+            Write("Введите колличество карт - картинок в руке");
+            int icons = int.Parse(ReadLine());
+            int summ = 0;
+            for (int i = 0; i != quantity; i++)
+            {
+                Write("Введите вес карты");
+                summ = summ + int.Parse(ReadLine());
+            }
+            WriteLine(summ + icons * 10); // зачем прописывать 21 кейс, если это только увеличит код. Я усвоил оператор swift, но в этом задании им делать - копипастить строки
+            ReadKey();
+
+        }
+    }
+}
